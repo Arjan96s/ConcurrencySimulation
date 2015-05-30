b2b27a2ca38756bac7e1506ef6f93adfed2abccb
Arjan Schouten <arjan-schouten@live.nl>
2015-05-30 21:14:52 +0200

    Fixed bug in ddl

--- ddl.sql
+++ ddl.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE IF NOT EXISTS `opdracht2`;
-USE `opdracht2`;
+CREATE DATABASE IF NOT EXISTS `opdracht2-arjan`;
+USE `opdracht2-arjan`;
 
 CREATE TABLE IF NOT EXISTS `product` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `instock` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CHECK (instock <= 100)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `stock` (
@@ -14,5 +15,5 @@ CREATE TABLE IF NOT EXISTS `stock` (
   `mutation_date` datetime NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`product_id`,`mutation_amount`,`mutation_date`),
-  CONSTRAINT `product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)opdracht2
+  CONSTRAINT `product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
 ) ENGINE=InnoDB;
